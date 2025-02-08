639a51e6ea78949b2781e71d6823784565990ac1
Haopeng Zeng <haopengz@usc.edu>
2025-02-07 16:31:54 -0800

    updated ids_hw

--- ids_hw_modified/src/ids.v
+++ ids_hw_modified/src/ids.v
@@ -143,8 +143,8 @@ module ids
    generic_regs
    #( 
       .UDP_REG_SRC_WIDTH   (UDP_REG_SRC_WIDTH),
-      .TAG                 (`CORE_BLOCK_ADDR),          // Tag -- eg. MODULE_TAG
-      .REG_ADDR_WIDTH      (`CORE_REG_ADDR_WIDTH),     // Width of block addresses -- eg. MODULE_REG_ADDR_WIDTH
+      .TAG                 (`IDS_BLOCK_ADDR),          // Tag -- eg. MODULE_TAG
+      .REG_ADDR_WIDTH      (`IDS_REG_ADDR_WIDTH),     // Width of block addresses -- eg. MODULE_REG_ADDR_WIDTH
       .NUM_COUNTERS        (0),                 // Number of counters
       .NUM_SOFTWARE_REGS   (3),                 // Number of sw regs
       .NUM_HARDWARE_REGS   (1)                  // Number of hw regs
